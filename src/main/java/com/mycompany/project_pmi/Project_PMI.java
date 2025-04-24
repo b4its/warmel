547298475e47288b27cd78e5b5c081e11a919876
= <=>
2025-04-24 14:01:16 +0800

    java first project

---
+++ src/main/java/com/mycompany/project_pmi/Project_PMI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_pmi;
+
+/**
+ *
+ * @author dicky
+ */
+public class Project_PMI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
